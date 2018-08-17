d83007b9a008372405eee0cb3dd3b4747d7b2358
hrbrmstr <bob@rud.is>
2018-08-17 16:08:00 -0400

    R package repo initialization complete

---
+++ R/rrecog-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/rrecog>
+#' - BugReports: <https://gitlab.com/hrbrmstr/rrecog/issues>
+#' 
+#' @md
+#' @name rrecog
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
